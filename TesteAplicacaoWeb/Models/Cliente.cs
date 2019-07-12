7729704771a753dddd00e202b95c27063137e172
DANIEL <DANIEL@DANIEL-PC>
2019-07-12 16:51:36 -0300

    Primeiro Commit TesteAplicacaoWeb

---
+++ TesteAplicacaoWeb/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteAplicacaoWeb.Models
+{
+    public class Cliente
+    {
+           public string Nome
+        {
+            get;
+            set;
+        }
+        public Cliente(string texto)
+        {
+            this.Nome = texto;
+        }
+    }
+}
